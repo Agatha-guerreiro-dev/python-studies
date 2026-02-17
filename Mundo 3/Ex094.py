c9159b9e47c079ebcb11ae20bfd95c36d9f6fabb
Agatha-guerreiro-dev <agatha.guerreiro97@gmail.com>
2026-02-17 15:36:51 -0300

    git commit -m "Ex094 finished"

--- Mundo 3/Ex094.py
+++ Mundo 3/Ex094.py
@@ -9,9 +9,9 @@ B) The average age of the group.
 C) A list containing all the Women (W) registered.
 D) A list of all people with an age above the average.
 """
-from operator import itemgetter
 data_base = []
 person = {}
+age_compiler = []
 while True:
     person['name'] = input('Name: ').strip().title()
     while True:
@@ -20,7 +20,9 @@ while True:
             break
         else:
             print('Please, select a valid option.')
-    person['age'] = int(input('Age: '))
+    age = int(input('Age: '))
+    person['age'] = age
+    age_compiler.append(age)
     data_base.append(person.copy())
     while True:
         menu = input('Want to continue? [Y/N] ').strip().lower()
@@ -31,4 +33,16 @@ while True:
     if menu in 'n':
         break
 people_total = len(data_base)
-
+mean = sum(age_compiler) / people_total
+print('-=' * 20)
+print(f'The group has {people_total} people.')
+print(f'The average age of the group is {mean:.0f} years old')
+print('List of registered women: ')
+for d in data_base:
+    if d['gender'] == 'W':
+        print(f'-> {d["name"]}')
+print(f'List of people older than the average age:')
+for d in data_base:
+    if d['age'] > mean:
+        print(f'Name: {d["name"]}; Gender: {d["gender"]}; Age: {d["age"]}.')
+print('Finish')
